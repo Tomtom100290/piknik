bb0f31001f1a225f5a015ea01e0272ec0904e76a
Tomtom100290 <tom.n974@yahoo.com>
2025-12-15 14:42:36 +0400

    Mise à jour page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\FavorisRepository;
+use App\Repository\LieuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,19 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function indexHome(FavorisRepository $favorisRepository, LieuRepository $lieuRepository): Response
     {
+        $user = $this->getUser(); // récupère l'utilisateur connecté
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté.');
+        }
+
+        // $user est typé UserInterface, getId() fonctionne si User a la méthode getId()
+        $favoris = $favorisRepository->findBy(['fkUser' => $user]);
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
+            'lieus' => $lieuRepository->findAll(),
+            'favoris' => $favoris,
         ]);
     }
 }
